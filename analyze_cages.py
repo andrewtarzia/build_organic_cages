d26622fa92e27ec4c187cef6d5662d7911cd38d9
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-02-08 19:06:48 +0000

    write cage analysis (wip)

--- analyze_cages.py
+++ analyze_cages.py
@@ -3,7 +3,7 @@
 # Distributed under the terms of the MIT License.
 
 """
-Script to build simple imine organic cages.
+Script to analyze simple imine organic cages.
 
 Author: Andrew Tarzia
 
@@ -13,8 +13,11 @@ Date Created: 07 Feb 2020
 
 import sys
 import stk
-from os import getcwd, mkdir
-from os.path import exists, join
+from os.path import exists
+
+from utilities import load_cage_list, update_cage_json
+import cage_analysis
+
 
 def main():
     """
@@ -22,15 +25,11 @@ def main():
 
     """
     usage_string = (
-        'analyze_cages.py settings_file '
-        'cage_list_file'
+        'analyze_cages.py cage_list_file'
     )
-    if (not len(sys.argv) == 6):
+    if (not len(sys.argv) == 2):
         print(f"""
 Usage: {usage_string}
-    settings_file : str
-        File with settings for code.
-
     cage_list_file :
         .json file to populate with cage definitions of form:
         `name`,`alde`,`ami`,`topology`,`alde`
@@ -38,8 +37,7 @@ Usage: {usage_string}
 """)
         sys.exit()
     else:
-        settings = read_settings(sys.argv[1])
-        cage_list_file = sys.argv[2]
+        cage_list_file = sys.argv[1]
 
     # Define list of cages to be built.
     # Load this file if it exists to avoid rebuilding cages.
@@ -50,6 +48,51 @@ Usage: {usage_string}
         raise FileNotFoundError(
             f'{cage_list_file} not found. Build cages!'
         )
+
+    print(f'.............{len(cage_dct)} cages to analyse...........')
+
+    # Build and optimise cages.
+    for cage_name in cage_dct:
+        print(cage_name)
+        cd = cage_dct[cage_name]
+        opt_mol = f'{cage_name}_opt.mol'
+        opt_json = f'{cage_name}_opt.json'
+        pw_json = f'{cage_name}_opt.json'
+        pw_xyz = f'{cage_name}_opt_pwout.xyz'
+        # If analysed, then skip.
+        if 'analysed' in cd.keys() and cd['analysed']:
+            continue
+        else:
+            cd['analysed'] = False
+        print(cd)
+
+        # If output file of optimization is present, then analyse.
+        if exists(opt_json):
+            print(f'.............pyWindow analysis of: {cage_name}...')
+            cage_analysis.pw_analyze_cage_from_MOL(
+                file=opt_mol,
+                prop_out=pw_json,
+                xyz_out=pw_xyz,
+                include_coms=True
+            )
+        # If pywindow output file is present then save analysis to dct.
+        if exists(pw_json):
+            # Read in cage structure.
+            cage = stk.ConstructedMolecule.load(opt_json)
+            cd = cage_analysis.assign_cage_properties(
+                dct=cd,
+                cage=cage,
+                pw_file=pw_json
+            )
+            cd['analysed'] = True
+
+        print(cd)
+        # Update cage dict file.
+        cage_dct[cage_name] = cd
+        update_cage_json(cage_dct, cage_list_file)
+        print(cage_dct)
+        sys.exit('check that this all matches files')
+
 
 if __name__ == "__main__":
     main()
--- cage_analysis.py
+++ cage_analysis.py
@@ -10,3 +10,181 @@ Author: Andrew Tarzia
 Date Created: 07 Feb 2020
 
 """
+
+import json
+import numpy as np
+import pywindow as pw
+from rdkit.Chem import AllChem as Chem
+
+
+def pw_analyze_cage(
+    cage,
+    propfile=None,
+    structfile=None,
+    include_coms=True
+):
+    """
+    Analyze cage already loaded into pywindow.
+
+    """
+    # Perform full pyWindow analysis
+    cage.full_analysis()
+    # Dump pyWindow properties into JSON and cage into xyz
+    if propfile is not None:
+        cage.dump_properties_json(propfile)
+    if structfile is not None:
+        cage.dump_molecule(structfile, include_coms=include_coms)
+
+
+def pw_analyze_cage_from_MOL(
+    file,
+    prop_out,
+    xyz_out,
+    include_coms=True
+):
+    """
+    Run all desired analysis on a single built cage molecule.
+
+    Output cage with COM atoms to XYZ and properties to JSON.
+
+    """
+    # Import optimised cage into pyWindow, via RDkit mol file
+    cage_rd = Chem.MolFromMolFile(file)
+    cage_sys = pw.MolecularSystem.load_rdkit_mol(cage_rd)
+    cage_mol = cage_sys.system_to_molecule()
+    pw_analyze_cage(
+        cage=cage_mol,
+        propfile=prop_out,
+        structfile=xyz_out,
+        include_coms=include_coms
+    )
+
+
+def topo_2_property(topology, property):
+    """
+    Returns properties of a topology for a given topology name.
+
+    Properties:
+        'stoich' - gives the stoichiometries of both building blocks
+            assuming that the first building block has the larger
+            number of functional groups.
+        'noimines' - gives the number of imines formed to build that
+            topology
+        'expected_wind' - gives the number of windows expected
+
+    Currently defined topologies:
+        TwoPlusThree topologies
+        ThreePlusThree topologies
+
+    """
+    properties = ['stoich', 'noimines', 'expected_wind']
+    if property not in properties:
+        raise ValueError(
+            f'{property} not defined'
+            f'possible properties: {properties}'
+        )
+
+    dict = {
+        '2p3': {
+            'stoich': (2, 3),
+            'noimines': 6,
+            'expected_wind': 3,
+        },
+        '4p6': {
+            'stoich': (4, 6),
+            'noimines': 12,
+            'expected_wind': 4,
+        },
+        '4p62': {
+            'stoich': (4, 6),
+            'noimines': 12,
+            'expected_wind': 4,
+        },
+        '6p9': {
+            'stoich': (6, 9),
+            'noimines': 18,
+            'expected_wind': 5,
+        },
+        '1p1': {
+            'stoich': (1, 1),
+            'noimines': 3,
+            'expected_wind': 3,
+        },
+        '4p4': {
+            'stoich': (4, 4),
+            'noimines': 12,
+            'expected_wind': 6,
+        },
+    }
+    if topology not in dict:
+        raise ValueError(f'properties not defined for {topology}')
+    return dict[topology][property]
+
+
+def is_collapsed(topo, pore_diameter, no_window):
+    """
+    Returns True if a cage is deemed to be collapsed.
+
+    A collapsed cage is defined as having:
+        - pore_diam_opt < 2.8 Angstrom (H2 kinetic diameter)
+        - number of windows != expected number based on topology.
+
+    """
+    expected_wind = topo_2_property(topo, property='expected_wind')
+    if expected_wind != no_window:
+        return True
+    elif pore_diameter < 2.8:
+        return True
+    else:
+        return False
+
+
+def assign_cage_properties(dct, cage, pw_file):
+    '''
+    Assigns and outputs arbitrary cage properties to dct.
+
+    '''
+
+    with open(pw_file, 'r') as f:
+        pwdata = json.load(f)
+
+    # Get pywindow data.
+    dct['max_diam'] = pwdata['maximum_diameter']['diameter']
+    dct['p_diam'] = pwdata['pore_diameter']['diameter']
+    dct['p_diam_opt'] = pwdata['pore_diameter_opt']['diameter']
+    dct['p_vol'] = pwdata['pore_volume']
+    dct['p_vol_opt'] = pwdata['pore_volume_opt']
+    if pwdata['windows']['diameters'] is None:
+        window_data = None
+    elif len(pwdata['windows']['diameters']) == 0:
+        window_data = None
+    else:
+        window_data = {
+            'w_no': len(pwdata['windows']['diameters']),
+            'w_max': max(pwdata['windows']['diameters']),
+            'w_min': min(pwdata['windows']['diameters']),
+            'w_avg': np.average(pwdata['windows']['diameters'])
+        }
+    dct['windows'] = window_data
+
+    # Following structural analysis fails if pyWindow bug is
+    # encountered. The bug produces overly large windows.
+    # Do not do analysis and assume collapsed if big occurs.
+    if max(pwdata['windows']['diameters']) < 200:
+        # Get window differences if topology is not 4p62.
+        if dct['topo'] != '4p62':
+            dct['w_diff'] = cage.window_difference()
+        else:
+            dct['w_diff'] = None
+
+        # Get collapsed flag.
+        dct['coll_flag'] = is_collapsed(
+            topo=dct['topo'],
+            pore_diameter=dct['p_diam_opt'],
+            no_window=dct['windows']['w_no']
+        )
+    else:
+        dct['w_diff'] = None
+        dct['coll_flag'] = True
+
+    return dct
